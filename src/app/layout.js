2273c1b2a551fcf8aae0881f0151f462c613541f
Yoshiteru Nagata <yositeru1108@nagoya-u.jp>
2024-11-18 20:23:33 +0900

    fix some configs / codes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -13,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="jp">
       <head>
-      <script src="http://localhost:8097"></script>
+      <Script src="http://localhost:8097"></Script>
       </head>
       <body
         className={inter.className}
